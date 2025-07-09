f45316e97541c23ebba4a74f2ca494c3ec4f7ce2
Brucy666 <robleach16@icloud.com>
2025-07-10 00:58:29 +0100

    Update sniper_data.py

--- sniper_data.py
+++ sniper_data.py
@@ -2,19 +2,17 @@ import requests
 import os
 
 def get_bybit_price_vwap():
-    url = "https://coinalyze-api.p.rapidapi.com/v1/market-data"
+    url = "https://coinalyze-api.p.rapidapi.com/futures/summary"
     headers = {
         "X-RapidAPI-Key": os.getenv("COINALYZE_API_KEY"),
         "X-RapidAPI-Host": "coinalyze-api.p.rapidapi.com"
     }
-    params = {
-        "symbol": "BYBIT:BTCUSD"
-    }
+    params = {"symbol": "BYBIT:BTCUSD"}
 
     try:
         response = requests.get(url, headers=headers, params=params, timeout=5)
         data = response.json()
-        print("🔘 Raw Coinalyze Data:", data)
+        print("🟤 Raw Coinalyze Data", data)
 
         price = float(data["lastPrice"])
         vwap = float(data["vwap"])
