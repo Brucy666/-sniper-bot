e7c6bd63fae43caa94ad94970ad1e1ba17193c96
Brucy666 <robleach16@icloud.com>
2025-07-10 10:07:46 +0100

    Update sniper_data.py

--- sniper_data.py
+++ sniper_data.py
@@ -2,14 +2,14 @@ import requests
 
 def get_bybit_price_vwap():
     api_key = "671f76e0-53d2-432b-a57a-afbd6d89fd48"
-    url = f"https://coinalyze.net/api/v1/bybit/futures/get-spot-summary?symbol=BYBIT:BTCUSD&api_key={api_key}"
+    url = "https://coinalyze.net/api/v1/bybit/futures/summary?symbol=BTCUSD.P&api_key=" + api_key
 
     try:
         response = requests.get(url, timeout=5)
         data = response.json()
         print("🧠 Raw Coinalyze Data", data)
 
-        price = float(data["price"])
+        price = float(data["lastPrice"])
         vwap = float(data["vwap"])
 
         return price, vwap
