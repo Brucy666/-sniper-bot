fea78f381e1041b7795140545f10322d53394b2e
Brucy666 <robleach16@icloud.com>
2025-07-10 10:08:33 +0100

    Update sniper_data.py

--- sniper_data.py
+++ sniper_data.py
@@ -1,8 +1,8 @@
 import requests
+import os
 
 def get_bybit_price_vwap():
-    api_key = "671f76e0-53d2-432b-a57a-afbd6d89fd48"
-    url = "https://coinalyze.net/api/v1/bybit/futures/summary?symbol=BTCUSD.P&api_key=" + api_key
+    url = "https://coinalyze.net/api/v1/bybit/futures/summary?symbol=BTCUSD.P&api_key=671f76e0-53d2-432b-a57a-afbd6d89fd48"
 
     try:
         response = requests.get(url, timeout=5)
